c67a73a78539df19fe873bb9d0d30c2061de317b
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-06-20 23:18:34 +0400

    Fixes #13: showing invisible symbols in post editor.

--- scripts/editor.js
+++ scripts/editor.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var editor = ace.edit('editor');
 	editor.setTheme('ace/theme/twilight');
 	editor.getSession().setMode('ace/mode/markdown');
+	editor.setShowInvisibles(true);
 
 	$('#post-form').submit(function() {
 		$('#Post_text').val(editor.getValue());
