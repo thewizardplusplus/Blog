38aee6dcaf3c3d3cfd8c52b0d2c3a8958bd15e60
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-06-28 18:52:08 +0400

    Fixes #22: add GitHub widget.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -161,6 +161,15 @@
 						</a>
 						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 					</div>
+
+					<div>
+						<script
+							src = "http://nodejs.in/octocard/bin/octocard.js"
+							data-name = "thewizardplusplus"
+							data-modules = "base,repos,eventsStatis"
+							data-reposNum = "100">
+						</script>
+					</div>
 				</div>
 			</div>
 			<?php } ?>
