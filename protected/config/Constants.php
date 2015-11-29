7a77071f2030333eafce4fef109b520e13fdae6d
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-11-29 07:41:40 +0300

    Add missed constant.

--- protected/config/Constants.php
+++ protected/config/Constants.php
@@ -8,6 +8,8 @@ class Constants {
 	const COPYRIGHT_START_YEAR =          2013;
 	const MAXIMAL_LENGTH_OF_TITLE_FIELD = 255;
 	const CUT_TAG_PATTERN =               '/<cut\s*\/>/';
+	// 30 days
+	const REMEMBER_DURATION_IN_S = 2592000;
 	// relatively at /protected/controllers
 	const FILES_RELATIVE_PATH =           '/../../files';
 	// relatively at /protected/controllers
