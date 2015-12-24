130ea034cbfb6bbcbca0c725885618aa338c2248
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-12-24 14:21:14 +0300

    Issue #63: add XML reading/writing.

--- tools/dump_decoder.py
+++ tools/dump_decoder.py
@@ -11,15 +11,17 @@ Options:
 
 import docopt
 import os.path
+import xml.dom.minidom
+import sys
 
 def parse_options():
 	script_name = os.path.basename(__file__)
 	return docopt.docopt(__doc__.format(script_name))
 
 def process_options(options):
-	if options["<decoded-dump-file>"] == None:
-		decoded_dump_file = os.path.basename(options["<dump-file>"])
-		options["<decoded-dump-file>"] = decoded_dump_file
+	if options['<decoded-dump-file>'] == None:
+		decoded_dump_file = os.path.basename(options['<dump-file>'])
+		options['<decoded-dump-file>'] = decoded_dump_file
 
 def parse_parameters():
 	options = parse_options()
@@ -27,5 +29,22 @@ def parse_parameters():
 
 	return options
 
+def read_xml(filename):
+	return xml.dom.minidom.parse(filename)
+
+def write_xml_to_file(dom, filename):
+	with open(filename, 'w') as target_file:
+		dom.writexml(target_file, encoding = 'utf-8')
+
+def write_xml_to_stdout(dom):
+	dom.writexml(sys.stdout, encoding = 'utf-8')
+
+def write_xml(dom, target):
+	if target != "-":
+		write_xml_to_file(dom, target)
+	else:
+		write_xml_to_stdout(dom)
+
 parameters = parse_parameters()
-print(parameters)
+dom = read_xml(parameters['<dump-file>'])
+write_xml(dom, parameters['<decoded-dump-file>'])
