0239baa46119bfa4af22866cc1c50d38cd804c0d
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-12-30 06:23:16 +0300

    Issue #64: add CSV writing.

--- tools/dump_to_csv.py
+++ tools/dump_to_csv.py
@@ -12,6 +12,8 @@ Options:
 import docopt
 import os.path
 import xml.dom.minidom
+import csv
+import sys
 
 def parse_options():
 	script_name = os.path.basename(__file__)
@@ -36,13 +38,37 @@ def find_posts(node):
 	return node.getElementsByTagName('post')
 
 def prepare_post(post):
-	return {'test 1': 'ololo 1', 'test 2': 'ololo 2'}
+	return {'test 1': '"ololo" 1', 'test 2': 'ololo\n2'}
 
 def prepare_posts(posts):
 	return map(prepare_post, posts)
+
+def write_csv_to_writer(writer, posts):
+	field_names = ['test 1', 'test 2']
+	writer = csv.DictWriter( \
+		writer, \
+		fieldnames = field_names, \
+		lineterminator = '\n')
+
+	writer.writeheader()
+	for post in posts:
+		writer.writerow(post)
+
+def write_csv_to_file(csv_filename, posts):
+	with open(csv_filename, 'w') as csv_file:
+		write_csv_to_writer(csv_file, posts)
+
+def write_csv_to_stdout(posts):
+	write_csv_to_writer(sys.stdout, posts)
+
+def write_csv(target, posts):
+	if target != "-":
+		write_csv_to_file(target, posts)
+	else:
+		write_csv_to_stdout(posts)
 
 parameters = parse_parameters()
 dom = read_xml(parameters['<dump-file>'])
 posts = find_posts(dom.documentElement)
 prepared_posts = prepare_posts(posts)
-print(prepared_posts)
+write_csv(parameters['<csv-file>'], prepared_posts)
