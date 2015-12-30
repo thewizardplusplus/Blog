cc015e5b74566aa5722d6903b7db7e0b90e182b9
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-12-30 07:08:28 +0300

    Issue #64: add general post preparing.

--- tools/dump_to_csv.py
+++ tools/dump_to_csv.py
@@ -9,11 +9,16 @@ Options:
   -h, --help  - show help.
 """
 
+import re
 import docopt
 import os.path
 import xml.dom.minidom
+import base64
+import slugify
 import csv
 import sys
+
+cut_tag_pattern = re.compile('<cut\s*\/>')
 
 def parse_options():
 	script_name = os.path.basename(__file__)
@@ -37,14 +42,60 @@ def read_xml(filename):
 def find_posts(node):
 	return node.getElementsByTagName('post')
 
+def get_subnode(node, subnode_tag):
+	return node.getElementsByTagName(subnode_tag)[0]
+
+def decode_text(text):
+	data = base64.b64decode(text)
+	data = data.decode('string_escape')
+	return data.strip()
+
+def get_node_text(nodes):
+	text = ''
+	for node in nodes.childNodes:
+		if node.nodeType == node.TEXT_NODE:
+			text += decode_text(node.data)
+
+	return text
+
+def extract_excerpt(text):
+	excerpt = cut_tag_pattern.split(text)[0]
+	return excerpt.strip()
+
+def extract_content(text):
+	return cut_tag_pattern.sub('', text)
+
+def get_post_tags(post):
+	tags = get_node_text(get_subnode(post, 'tags'))
+	return '|'.join(map(lambda tag: tag.strip(), tags.split(',')))
+
+def get_attribute(node, attribute_name):
+	return node.attributes[attribute_name].value
+
 def prepare_post(post):
-	return {'test 1': '"ololo" 1', 'test 2': 'ololo\n2'}
+	title = get_node_text(get_subnode(post, 'title'))
+	text = get_node_text(get_subnode(post, 'text'))
+	return { \
+		'Title': title, \
+		'Slug': slugify.slugify(title), \
+		'Excerpt': extract_excerpt(text), \
+		'Content': extract_content(text), \
+		'Tags': get_post_tags(post), \
+		'Created date': get_attribute(post, 'create-time'), \
+		'Updated date': get_attribute(post, 'modify-time')}
 
 def prepare_posts(posts):
 	return map(prepare_post, posts)
 
 def write_csv_to_writer(writer, posts):
-	field_names = ['test 1', 'test 2']
+	field_names = [ \
+		'Title', \
+		'Slug', \
+		'Excerpt', \
+		'Content', \
+		'Tags', \
+		'Created date', \
+		'Updated date']
 	writer = csv.DictWriter( \
 		writer, \
 		fieldnames = field_names, \
