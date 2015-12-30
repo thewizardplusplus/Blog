8fb8361438586452e463e0c12e8b5e8c8d62a84b
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-12-30 05:54:47 +0300

    Issue #64: add options parsing.

---
+++ tools/dump_to_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+Usage:
+  {0:s} -h | --help
+  {0:s} <dump-file> [<csv-file>]
+
+Options:
+  -h, --help  - show help.
+"""
+
+import docopt
+import os.path
+
+def parse_options():
+	script_name = os.path.basename(__file__)
+	return docopt.docopt(__doc__.format(script_name))
+
+def process_options(options):
+	if options['<csv-file>'] == None:
+		csv_file = os.path.basename(options['<dump-file>'])
+		csv_file = os.path.splitext(csv_file)[0]
+		options['<csv-file>'] = csv_file + '.csv'
+
+def parse_parameters():
+	options = parse_options()
+	process_options(options)
+
+	return options
+
+parameters = parse_parameters()
+print(parameters)
