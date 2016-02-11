537e0a69eea68712fc1b8e2ad148c99ad7642e74
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-02-11 17:08:59 +0300

    Issue #68: add a target creation.

--- tools/posts_collector.py
+++ tools/posts_collector.py
@@ -18,6 +18,11 @@ def parse_options():
     script_name = os.path.basename(__file__)
     return docopt.docopt(__doc__.format(script_name))
 
+def create_target(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 if __name__ == '__main__':
     options = parse_options()
+    create_target(options['<target-path>'])
     print(options)
